727911628c471f7add657ec10aecae670c18390a
react-city <art9024@gmail.com>
2019-04-15 22:01:31 +0300

    Заменил расширение js на jsx

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,10 +1,14 @@
 import React from 'react';
+import Counter from '../Counter/Counter';
 import './Header.css';
 
 export default class Header extends React.Component {
     render() {
         return (
-            <div className="header">Hello world!</div>
+            <div className="header">
+                Hello world!
+                <Counter />
+            </div>
         );
     }
 }
