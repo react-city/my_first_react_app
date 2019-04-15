059eef0b5585c8d5f43d2e944116ca959c53045a
react-city <art9024@gmail.com>
2019-04-15 21:17:42 +0300

    добавил компонент Header

---
+++ src/components/Header/Header.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import './Header.css';
+
+export default class Header extends React.Component {
+    render() {
+        return (
+            <div className="header">Hello world!</div>
+        );
+    }
+}
