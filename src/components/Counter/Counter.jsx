7d7d57e4929e7504e6053e7d2d65c278c018eba1
react-city <art9024@gmail.com>
2019-04-15 22:01:51 +0300

    Новый компонент Counter

---
+++ src/components/Counter/Counter.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import './Counter.css';
+
+export default class Counter extends React.Component {
+    state = { counter: 0 };
+
+    componentDidMount() {
+        setInterval(this.incrementCounter, 1000);
+    }
+    
+    incrementCounter = () => {
+        this.setState({ counter: this.state.counter + 1 });
+    }
+
+    render() {
+        return (
+            <div className="counter">{this.state.counter}</div>
+        );
+    }
+}
