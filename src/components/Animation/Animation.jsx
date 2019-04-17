001a429b9f2cbc9771763301597273efac9c7cec
evstarostin89 <evstarostin89@yandex-team.ru>
2019-04-17 22:32:29 +0300

    анимация

---
+++ src/components/Animation/Animation.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import './Animation.css';
+
+export default class Animation extends React.Component {
+    render() {
+        return (
+            <div className="animation" />
+        );
+    }
+}
