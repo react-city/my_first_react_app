4db563c85f3f159a6859cd370a6928857c9a0f5e
evstarostin89 <evstarostin89@yandex-team.ru>
2019-04-17 22:32:47 +0300

    Updates

--- precache-manifest.815de2bbf52cc8f2822b67b6a37b503a.js
+++ precache-manifest.815de2bbf52cc8f2822b67b6a37b503a.js
@@ -4,19 +4,19 @@ self.__precacheManifest = [
     "url": "/my_first_react_app/static/js/runtime~main.e04968da.js"
   },
   {
-    "revision": "758b966e2ead8f61dccb",
-    "url": "/my_first_react_app/static/js/main.63f7b767.chunk.js"
+    "revision": "01ec8e51a537da68c7ad",
+    "url": "/my_first_react_app/static/js/main.f47d094d.chunk.js"
   },
   {
     "revision": "9eee67cf782a38d35aeb",
     "url": "/my_first_react_app/static/js/2.d6a534e5.chunk.js"
   },
   {
-    "revision": "758b966e2ead8f61dccb",
-    "url": "/my_first_react_app/static/css/main.782580e1.chunk.css"
+    "revision": "01ec8e51a537da68c7ad",
+    "url": "/my_first_react_app/static/css/main.dd7a0665.chunk.css"
   },
   {
-    "revision": "e96bc91e14b5bf9ad1690d775eae6775",
+    "revision": "4ce3f6b60764779ece8cc7fb2f674332",
     "url": "/my_first_react_app/index.html"
   }
 ];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/my_first_react_app/precache-manifest.c44275aeafea661371075d9fb44e25d9.js"
+  "/my_first_react_app/precache-manifest.815de2bbf52cc8f2822b67b6a37b503a.js"
 );
 
 workbox.clientsClaim();
